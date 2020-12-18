a2204cc0fc206191ae5fb7945b7137e48019e63d
Keith Filkins <kafilkins@me.com>
2020-12-18 22:49:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,7 @@ class Artist
     @@all
   end
 
-  def self.find_or_create_by_name(name)
+  def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
       found_artist
